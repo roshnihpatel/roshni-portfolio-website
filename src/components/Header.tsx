e551dff4cecb325bbdc222caffc3bd4168791441
Roshni Patel <roshnihpatel@outlook.com>
2022-06-10 18:11:18 +0100

    added navigation bar

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,39 @@
 export function Header(): JSX.Element {
   return (
-    <>
-      <h2>header</h2>
-    </>
+    <header>
+      <div className="logo">
+        <i className="fa-solid fa-bars"></i>
+      </div>
+      <button className="nav-toggle" aria-label="toggle navigation">
+        <span className="hamburger-menu"></span>
+      </button>
+      <nav className="nav">
+        <ul className="nav__list">
+          <li className="nav__item">
+            <a
+              className="nav__link"
+              href="https://www.w3schools.com/css/css_rwd_viewport.asp"
+            >
+              home
+            </a>
+          </li>
+          <li className="nav__item">
+            <a className="nav__link" href="#AboutMe">
+              About Me
+            </a>
+          </li>
+          <li className="nav__item">
+            <a className="nav__link" href="#MyWork">
+              My Work
+            </a>
+          </li>
+          <li className="nav__item">
+            <a className="nav__link" href="#Footer">
+              Contact Me
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </header>
   );
 }
